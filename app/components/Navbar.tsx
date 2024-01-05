c7aee91e1eceef7d82d48b9e142a313155fed1b4
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-01-05 20:26:20 +0200

    Build Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+import logo from '@/public/assets/logo.jpeg'
+import Link from 'next/link'
+import menuLinks from '../constants/menuLinks'
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center py-2 px-4 border-b border-slate-200">
+      <div>
+        <Link href="/">
+          <Image src={logo} alt="logo" className="w-10" />
+        </Link>
+      </div>
+      <ul className="space-x-8 ">
+        {menuLinks.map((menu, index) => (
+          <Link key={index} href={menu.href} className=" hover:text-yellow-300">
+            {menu.name}
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ app/constants/menuLinks.ts
@@ -0,0 +1,20 @@
+const menuLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About us",
+    href: "/",
+  },
+  {
+    name: "Portfolio",
+    href: "/",
+    },
+    {
+        name: "Contact us",
+        href: "/"
+  }
+];
+
+export default menuLinks;
